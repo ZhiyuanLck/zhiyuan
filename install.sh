d77443380f5e9f980b2cb006baa8b3361f16f7ce
ZhiyuanLck <L2517996083@163.com>
2021-03-04 19:23:00 +0800

    add alacritty config

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ git submodule update --init --recursive
 
 echo "configuring zsh..."
 ./zsh/bootstrap.zsh
+
+echo "configuring alacritty..."
+local alacritty_dir=~/.config/alacritty
+local alacritty_config=$zhiyuan/alacritty/alacritty.yml
+mkdir -p $alacritty_dir
+[[ -e $alacritty_config ]] && cp $alacritty_config $alacritty_dir
